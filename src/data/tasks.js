a08bd87b19b308b355e7fde38a00c28df0abf31f
dan25g <dangar452000@gmail.com>
2023-10-12 15:36:15 -0400

    firts commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "primera tarea",
+    description: "Mi primera Tarea",
+  },
+  {
+    id: 1,
+    title: "Segunda tarea",
+    description: "Mi segunda Tarea",
+  },
+  {
+    id: 2,
+    title: "Tercera tarea",
+    description: "Mi tercera Tarea",
+  },
+];
